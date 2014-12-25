67ac4a1da21faf7a2ecfd37b4a1de5616fa7a535
jjcrochemar <jjc@kpassit.com>
2014-12-25 17:11:56 +0100

    new package added

---
+++ GitUsagePJ1/src/com/myclasses/myfirstclass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.myclasses;
+
+/**
+ * @author jjcrochemar
+ *
+ */
+public class myfirstclass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
