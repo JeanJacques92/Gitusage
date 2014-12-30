a484b6c7e020334d739ada726a22dff9f8672c9d
JeanJacques92 <jjcrochemar@gmail.com>
2014-12-30 14:19:19 +0100

    commit 1

--- GitUsagePJ1/src/gitusage.java
+++ GitUsagePJ1/src/gitusage.java
@@ -14,7 +14,7 @@ public class gitusage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("user 1 add");
-
+		//modify this
 	}
 	
 	public void doitagain() {
