ca4207da81a095c9eac16a41058f0a41e066340a
jjcrochemar <jjc@kpassit.com>
2015-01-05 16:15:03 +0100

    try push commit

---
+++ GitUsagePJ1/src/com/myclasses/fichiertwo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author jjcrochemar
+ *
+ */
+public class fichiertwo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
