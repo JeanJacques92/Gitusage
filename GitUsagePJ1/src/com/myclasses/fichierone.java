c8d8501a5f116e8d1c47f0d067f1844e8f9dc0b8
jjcrochemar <jjc@kpassit.com>
2015-01-05 15:50:18 +0100

    fichier two

---
+++ GitUsagePJ1/src/com/myclasses/fichierone.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.myclasses;
+
+/**
+ * @author jjcrochemar
+ *
+ */
+public class fichierone {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
